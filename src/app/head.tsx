0272a807fae396cde9e214104ddb5eabed420601
wu0000-zoho <wu0000@zohomail.com>
2026-08-27 23:29:09 +0800

    chore: add basic app/head placeholder to include favicon

---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <link rel="icon" href="/icon.png" />
+    <title>Beijing Xnavor — Headless resources and services</title>
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
